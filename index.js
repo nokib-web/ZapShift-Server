3f2b08016ba671370cc481a3d826cfab87a62d6f
nokib-web <nokibnokib1@gmail.com>
2025-11-27 19:04:59 +0600

    add rider and user api

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const crypto = require("crypto");
 const admin = require("firebase-admin");
 
- const serviceAccount = require("./zapshift-firebase-adminsdk.json");
-
-
-
+const serviceAccount = require("./zapshift-firebase-adminsdk.json");
 
 const port = process.env.PORT || 3000
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 function generateTrackingId() {
     const bytes = crypto.randomBytes(6); // returns a Buffer
@@ -25,19 +27,21 @@ function generateTrackingId() {
 app.use(express.json());
 app.use(cors())
 
-const verifyFBToken = async(req, res, next)=>{
-    // console.log("headers in the middleware",req.headers.authorization)
+const verifyFBToken = async (req, res, next) => {
+    // console.log("headers in the middleware",req.headers.Authorization)
     const token = req.headers.authorization;
-    if(!token){
-        return res.status(401).send({message: 'unauthorized access'})
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
     }
-    try{
+    try {
         const idToken = token.split(' ')[1];
         const decoded = await admin.auth().verifyIdToken(idToken)
-        console.log('decoded dta',decoded)
-    }
-    catch(err){
+        req.decoded_email = decoded.email;
+        console.log('decoded dta', decoded)
 
+    }
+    catch (err) {
+        return res.status(401).send({ message: "unauthorized access" })
     }
 
 
@@ -66,9 +70,107 @@ async function run() {
         await client.connect();
 
         const db = client.db("ZapShift");
-        const usersCollection = db.collection("users");
+        const userCollection = db.collection("users");
+        const riderCollection = db.collection("riders")
         const parcelsCollection = db.collection("parcels");
         const paymentCollection = db.collection('payments');
+
+        // Users related api
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            user.role = 'user';
+            user.createdAt = new Date();
+
+            const email = user.email
+            const userExist = await userCollection.findOne({ email })
+            if (userExist) {
+                return res.send({ message: 'user already exist' })
+            }
+            const result = await userCollection.insertOne(user);
+            res.json(result); // json => send
+        })
+
+        // Riders related api
+
+        // Get rider
+        app.get('/riders', async (req, res) => {
+            const query = {}
+            if (req.query.status) {
+                query.status = req.query.status
+            }
+            const cursor = riderCollection.find(query)
+            const result = await cursor.toArray()
+            res.json(result)
+        })
+
+        // Create rider
+        app.post('/riders', async (req, res) => {
+            const rider = req.body;
+            rider.status = 'pending';
+            rider.createdAt = new Date();
+
+
+            const email = rider.email
+            const riderExist = await riderCollection.findOne({ email })
+            if (riderExist) {
+                return res.status(409).json({ message: 'rider already exist' })
+            }
+
+
+            const result = await riderCollection.insertOne(rider);
+            res.json(result);
+        })
+
+        // update rider
+
+        app.patch('/riders/:id', verifyFBToken, async (req, res) => {
+            const status = req.body.status;
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+
+            const result = await riderCollection.updateOne(query, updatedDoc)
+            if (status === 'approved') {
+                const email = req.body.email;
+                const userQuery = { email }
+                const updateUser = {
+                    $set: {
+                        role: 'rider'
+                    }
+                }
+
+                const userResult = await userCollection.updateOne(userQuery, updateUser)
+            }
+            res.json(result)
+
+        })
+
+        // Delete a Rider application 
+        // DELETE a rider by ID
+        app.delete("/riders/:id", verifyFBToken, async (req, res) => {
+            try {
+                const id = req.params.id;
+
+                const query = { _id: new ObjectId(id) };
+                const result = await riderCollection.deleteOne(query);
+
+                if (result.deletedCount === 0) {
+                    return res.status(404).send({ message: "Rider not found" });
+                }
+
+                res.send({ success: true, deleted: result });
+
+            } catch (error) {
+                console.error(error);
+                res.status(500).send({ message: "Internal Server Error", error: error.message });
+            }
+        });
+
 
         // get by id Api
         app.get('/parcels/:id', async (req, res) => {
@@ -117,12 +219,16 @@ async function run() {
             const email = req.query.email;
             const query = {}
 
-            // console.log(req.headers)
+            console.log(req.headers)
 
             if (email) {
                 query.customerEmail = email
+                // check email address
+                if (email !== req.decoded_email) {
+                    return res.status(403).send({ message: "forbidden access" })
+                }
             }
-            const cursor = paymentCollection.find(query)
+            const cursor = paymentCollection.find(query).sort({ paidAt: -1 })
             const result = await cursor.toArray();
             res.send(result);
         });
@@ -184,7 +290,6 @@ async function run() {
                                 name: ` Please pay for: ${paymentInfo.parcelName}`,
                             }
                         },
-
 
                         quantity: 1,
                     },
